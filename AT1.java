68e29143877fdbd0bf059eaa0d7ee50459e7f067
Troiando <jandilson.maia.jm@gmail.com>
2023-03-08 14:07:57 -0300

    Create AT1.java

---
+++ AT1.java
@@ -0,0 +1,17 @@
+import java.util.Scanner;
+/**
+ * AT1
+ */
+public class AT1 {
+
+    public static void main(String[] args) {
+        
+        Scanner sc = new Scanner(System.in);
+        String nome;
+
+        System.out.println("Insira o nome ");
+        nome = sc.nextLine();
+        System.out.print(nome);
+
+    }
+}
