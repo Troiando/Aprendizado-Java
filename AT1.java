96cb74690a82c6ad351619230e9e6352e12dbff8
Troiando <jandilson.maia.jm@gmail.com>
2023-03-08 14:33:00 -0300

    Update AT1.java
    ADD

--- AT1.java
+++ AT1.java
@@ -1,17 +1,24 @@
-import java.util.Scanner;
-/**
- * AT1
- */
 public class AT1 {
-
     public static void main(String[] args) {
-        
-        Scanner sc = new Scanner(System.in);
-        String nome;
+    
+        class Pessoa {
+            String nome;
+            int idade;
 
-        System.out.println("Insira o nome ");
-        nome = sc.nextLine();
-        System.out.print(nome);
+            Pessoa(String nome, int idade) {
+                this.nome = nome;
+                this.idade = idade;
+            }
 
+            void imprimir() {
+                    System.out.println(nome + " tem " + idade + " anos.");    
+            }
+        }
+
+        // Cria uma instância da classe Pessoa
+        Pessoa pessoa1 = new Pessoa("Jandilson", 20);
+        Pessoa pessoa2 = new Pessoa( "Alessandra", 21);
+        pessoa1.imprimir();
+        pessoa2.imprimir();
     }
 }
