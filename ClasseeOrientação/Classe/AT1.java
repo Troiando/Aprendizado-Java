ae74229f762afbb33dca399d4165da57cf1b9da8
Troiando <jandilson.maia.jm@gmail.com>
2023-04-05 20:18:45 -0300

    ADD

---
+++ ClasseeOrientação/Classe/AT1.java
@@ -0,0 +1,35 @@
+package ClasseeOrientação.Classe;
+
+public class AT1 {
+
+    
+public class Pessoa {
+
+    private String nome;
+    private int idade;
+    
+
+public Pessoa(String nome, int idade){
+    this.nome = nome;
+    this.idade = idade;
+}
+
+ public String getnome() {
+    return nome;    
+}
+
+ public void setNome(String nome) {
+    this.nome = "Jandilson";
+}
+
+
+}
+
+
+
+
+
+
+}
+
+
